4e4b7ce79a923af85e2e75046bb9f38eb70648a7
Sumit Maharjan <timusumit@gmail.com>
2018-11-17 16:29:04 +0545

    css removed unnecessary ones

--- application/views/site_templates/header.php
+++ application/views/site_templates/header.php
@@ -4,33 +4,33 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 <title>Kantipur Pest Control</title>
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/styles5560.css' type='text/css' media='all' />
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/css/frontend/animate09db.css' type='text/css' media='all' />
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/styles5560.css' type='text/css' media='all' /> -->
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/css/frontend/animate09db.css' type='text/css' media='all' /> -->
 <!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/css/fontawesome/css/font-awesome09db.css' type='text/css' media='all' /> -->
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/css/frontend/plugin_shortcodes9875.css' type='text/css' media='all' />
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/revslider/public/assets/css/settings23da.css?ver=5.4.8' type='text/css' media='all' />
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>framework/admin/css/datepicker09db.css' type='text/css' media='all' />
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/revslider/public/assets/css/settings23da.css' type='text/css' media='all' /> -->
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>framework/admin/css/datepicker09db.css' type='text/css' media='all' /> -->
 <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/style09db.css' type='text/css' media='all' />
 <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/sm-grid.css' type='text/css' media='all' />
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/prettyPhoto09db.css' type='text/css' media='all' />
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/prettyPhoto09db.css' type='text/css' media='all' /> -->
 <!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/fontawesome/css/font-awesome09db.css' type='text/css' media='all' /> -->
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/fontello/css/aicon_09db.css' type='text/css' media='all' />
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/fontello/css/animation09db.css' type='text/css' media='all' />
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/flexslider68b3.css?ver=1' type='text/css' media='all' /> 
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/fontello/css/aicon_09db.css' type='text/css' media='all' /> -->
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/fontello/css/animation09db.css' type='text/css' media='all' /> -->
+<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/flexslider68b3.css' type='text/css' media='all' /> 
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.7.1/flexslider.min.css">
 <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>css/responsive68b3.css?ver=1' type='text/css' media='all' />
-<link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/iva-business-hours-pro/assets/css/iva-bh-front09db.css' type='text/css' media='all' />
+<!-- <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/iva-business-hours-pro/assets/css/iva-bh-front09db.css' type='text/css' media='all' /> -->
 <link rel='stylesheet'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/jqueryui/jquery-ui.min.css' type='text/css' media='all' />
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery/jqueryb8ff.js?ver=1.12.4'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery/jquery-migrate.min330a.js'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/js/frontend/waypoints09db.js'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/mediaelement/mediaelement-and-player.min45a0.js?ver=4.2.6-78496d1'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/mediaelement/mediaelement-migrate.min09db.js'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/revslider/public/assets/js/jquery.themepunch.tools.min23da.js?ver=5.4.8'></script>
+<!-- <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/js/frontend/waypoints09db.js'></script> -->
+<!-- <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/mediaelement/mediaelement-and-player.min45a0.js?ver=4.2.6-78496d1'></script> -->
+<!-- <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/mediaelement/mediaelement-migrate.min09db.js'></script> -->
+<!-- <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/revslider/public/assets/js/jquery.themepunch.tools.min23da.js?ver=5.4.8'></script> -->
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/revslider/public/assets/js/jquery.themepunch.revolution.min23da.js?ver=5.4.8'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/jqueryui/jquery-ui.min.js'></script>
+<!-- <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/jqueryui/jquery-ui.min.js'></script> -->
 <link href="http://fonts.googleapis.com/css?family=Droid+Serif:400,700%7CLato:400,300,700,900,400italic,700italic,900italic%7CUbuntu:400,500,700%7C" rel="stylesheet" type="text/css" />
 </head>
 <body class="page-template page-template-template_stretched page-template-template_stretched-php page page-id-2678">
