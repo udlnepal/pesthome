cf4412989b82fa9f2b6bec1de7e206e1ad792ef8
Sumit Maharjan <timusumit@gmail.com>
2018-11-17 16:59:38 +0545

    jquery removed unnecessary

--- application/views/site_templates/footer.php
+++ application/views/site_templates/footer.php
@@ -70,33 +70,6 @@
 
 			</div><!-- .inner -->
 		</div><!-- .copyright -->
-<!-- <div class="footer-branches">
-				<div class="inner">
-					<aside id="text-4" class=" widget_text">
-						<div class="textwidget">
-							<div class="one_third">
-								<h5>United States</h5>
-									<p>15142, Collins Street West<br />
-										120 King Street, Melbourne<br />
-										Victoria 2000 Australia
-									</p>
-							</div>
-						<div class="one_third" >
-							<h5>United Kingdom</h5>
-							<p>15142, Collins Street West<br />
-								120 King Street, Melbourne<br />
-								Victoria 2000 Australia</p></div>
-						<div class="one_third last" >
-							<h5>Indonesia</h5>
-								<p>15142, Collins Street West<br />
-									120 King Street, Melbourne<br />
-									Victoria 2000 Australia
-								</p>
-						</div>
-						<div class="clear"></div>
-						</div>
-					</aside>
-				</div> -->
 </div>
 		
 </div><!-- #footer -->
@@ -109,20 +82,13 @@
 
 <link rel='stylesheet' id='anona-sc-owl-style-css'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/css/frontend/owl.carousel68b3.css?ver=1' type='text/css' media='all' />
 <link rel='stylesheet' id='anona-sc-owl-theme-css'  href='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/css/frontend/owl.theme68b3.css?ver=1' type='text/css' media='all' />
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/comment-reply.min09db.js'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery/ui/core.mine899.js?ver=1.11.4'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery/ui/datepicker.mine899.js?ver=1.11.4'></script>
-<!-- <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/mediaelement/wp-mediaelement.min09db.js'></script> -->
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery.easing.1.309db.js'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/hoverIntent09db.js'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/superfish09db.js'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery.preloadify.min09db.js'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery.prettyPhoto09db.js'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery.fitvids09db.js'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/sys_custom5152.js?ver=1.0'></script>
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/jquery.flexslider-min09db.js'></script>
-<script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/theme-love5152.js?ver=1.0'></script>
-<!-- <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>js/wp-embed.min09db.js'></script> -->
 <script type='text/javascript' src='<?php echo base_url('site_assets/'); ?>extra-plugin/anona-core/anona-shortcodes/js/frontend/owl.carousel09db.js'></script>
 
 </body>
