4fca1965380348a14f88cbe971165fafeda1445f
Sumit <timusumit@gmail.com>
2018-11-22 10:18:35 +0545

    fixed

--- application/views/admin_templates/sidebar.php
+++ application/views/admin_templates/sidebar.php
@@ -49,7 +49,7 @@ $classvalue=$this->uri->segment(2);
                 <b class="caret"></b>
               </p>
             </a>
-            <div class="collapse <?php if ($classvalue=='slider_setup' or $classvalue=='contact_setup' or $classvalue=='menu_setup' or $classvalue='services_setup')   echo "show"?>" id="pagesExamples" style="">
+            <div class="collapse <?php if ($classvalue=='slider_setup' or $classvalue=='contact_setup' or $classvalue=='menu_setup' or $classvalue=='services_setup')   echo "show"?>" id="pagesExamples" style="">
               <ul class="nav">
                 <li class="nav-item <?php if ($classvalue=='menu_setup') echo "active" ?> ">
                   <a class="nav-link" href="<?php echo base_url('admin/menu_setup'); ?>">
