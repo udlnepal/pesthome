78b2dc7942794eb4b8df5da9ab22cff8e1721a73
Sumit <timusumit@gmail.com>
2018-11-27 11:08:12 +0545

    pdf library added

---
+++ application/third_party/mpdf/ttfontdata/midas.mtx.php
@@ -0,0 +1,562 @@
+<?php
+$name='Raghindi';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 1241,
+  'XHeight' => 0,
+  'FontBBox' => '[-680 -496 1189 1339]',
+  'Flags' => 4,
+  'Ascent' => 1241,
+  'Descent' => -438,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 500,
+);
+$unitsPerEm=2048;
+$up=-65;
+$ut=10;
+$strp=259;
+$strs=50;
+$ttffile='D:/xampp/htdocs/hmis/app/third_party/mpdf/ttfonts/midas.ttf';
+$TTCfontID='0';
+$originalsize=134372;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='midas';
+$panose=' 0 0 0 0 4 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 1241, -438, 0
+// usWinAscent/usWinDescent = 1241, -438
+// hhea Ascent/Descent/LineGap = 1241, -438, 0
+$useOTL=255;
+$rtlPUAstr='';
+$GSUBScriptLang=array (
+  'deva' => 'DFLT ',
+);
+$GSUBFeatures=array (
+  'deva' => 
+  array (
+    'DFLT' => 
+    array (
+      'akhn' => 
+      array (
+        0 => 0,
+      ),
+      'nukt' => 
+      array (
+        0 => 1,
+      ),
+      'rphf' => 
+      array (
+        0 => 2,
+      ),
+      'blwf' => 
+      array (
+        0 => 3,
+      ),
+      'half' => 
+      array (
+        0 => 4,
+      ),
+      'vatu' => 
+      array (
+        0 => 5,
+        1 => 6,
+      ),
+      'pres' => 
+      array (
+        0 => 7,
+        1 => 8,
+        2 => 9,
+        3 => 10,
+        4 => 11,
+        5 => 12,
+      ),
+      'blws' => 
+      array (
+        0 => 13,
+        1 => 14,
+      ),
+      'abvs' => 
+      array (
+        0 => 15,
+        1 => 16,
+        2 => 17,
+        3 => 18,
+      ),
+      'psts' => 
+      array (
+        0 => 19,
+        1 => 20,
+        2 => 21,
+      ),
+      'haln' => 
+      array (
+        0 => 22,
+      ),
+    ),
+  ),
+);
+$GSUBLookups=array (
+  0 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 113730,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  1 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 113780,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  2 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 114228,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  3 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 114256,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  4 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 114284,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  5 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 115180,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  6 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 116076,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  7 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 116972,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  8 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 117290,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  9 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 117440,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  10 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 117484,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  11 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 117696,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  12 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 117890,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  13 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 118108,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  14 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 118760,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  15 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 118822,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  16 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 118994,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  17 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 119060,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  18 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 119474,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  19 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 119608,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  20 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 119802,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  21 => 
+  array (
+    'Type' => 6,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 119900,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  22 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 119998,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  23 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120752,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  24 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120770,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  25 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120788,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  26 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120806,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  27 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120824,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  28 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120850,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  29 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120868,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  30 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120910,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  31 => 
+  array (
+    'Type' => 1,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 120952,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+);
+$GPOSScriptLang=array (
+  'deva' => 'DFLT ',
+);
+$GPOSFeatures=array (
+  'deva' => 
+  array (
+    'DFLT' => 
+    array (
+      'abvm' => 
+      array (
+        0 => 0,
+        1 => 1,
+        2 => 2,
+        3 => 3,
+      ),
+      'blwm' => 
+      array (
+        0 => 4,
+        1 => 5,
+        2 => 6,
+      ),
+    ),
+  ),
+);
+$GPOSLookups=array (
+  0 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 121136,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  1 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 122038,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  2 => 
+  array (
+    'Type' => 2,
+    'Flag' => 256,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 122498,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  3 => 
+  array (
+    'Type' => 2,
+    'Flag' => 256,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 122888,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  4 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 123174,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  5 => 
+  array (
+    'Type' => 4,
+    'Flag' => 0,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 123986,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+  6 => 
+  array (
+    'Type' => 2,
+    'Flag' => 512,
+    'SubtableCount' => 1,
+    'Subtables' => 
+    array (
+      0 => 124230,
+    ),
+    'MarkFilteringSet' => '',
+  ),
+);
+?>
---
+++ application/third_party/mpdf/ttfontdata/preeti.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='Preeti';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 501,
+  'XHeight' => 500,
+  'FontBBox' => '[-427 -200 743 772]',
+  'Flags' => 4,
+  'Ascent' => 772,
+  'Descent' => -200,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 750,
+);
+$unitsPerEm=1000;
+$up=-133;
+$ut=20;
+$strp=259;
+$strs=50;
+$ttffile='F:/xampp/htdocs/work/hmis/app/third_party/mpdf/ttfonts/preeti.TTF';
+$TTCfontID='0';
+$originalsize=42376;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='preeti';
+$panose=' 0 0 0 0 0 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 750, 250, 0
+// usWinAscent/usWinDescent = 772, -200
+// hhea Ascent/Descent/LineGap = 772, -250, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
